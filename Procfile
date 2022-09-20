e5a6400af99382f919e7e99242e7ba796030b5a4
Conor <conor.ganly@yahoo.com>
2022-09-20 16:55:11 +0000

    Create procfile and add heroku to allowed hosts

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn my-roster-management.wsgi
--- myrostermanagement/settings.py
+++ myrostermanagement/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['my-roster-management.herokuapp.com', 'localhost']
 
 
 # Application definition
