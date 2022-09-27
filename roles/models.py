e07b0de20d8745c71f9fc593b8f780f4c54393e1
Conor <conor.ganly@yahoo.com>
2022-09-27 22:21:43 +0000

    Create and register model for roles

--- roles/models.py
+++ roles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Role(models.Model):
+    role_id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=50)
+    hourly_wage = models.DecimalField(null=True, blank=True, max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.title}"
