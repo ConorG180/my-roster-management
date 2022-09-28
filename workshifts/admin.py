440bedec7d19f0375d5685eba272542e1a03b787
Conor <conor.ganly@yahoo.com>
2022-09-28 01:14:36 +0000

    Create and register model for workshifts

--- workshifts/admin.py
+++ workshifts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Workshift
 # Register your models here.
+
+admin.site.register(Workshift)
--- workshifts/models.py
+++ workshifts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from roles.models import Role
+from employees.models import Employee
 # Create your models here.
+
+
+class Workshift(models.Model):
+    workshift_id = models.BigAutoField(primary_key=True)
+    employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="employee_id_workshift_set") 
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    role_id = models.ForeignKey(Role, on_delete=models.CASCADE, related_name="role_id_workshift_set") 
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.employee_id}: {self.start_time} - {self.end_time}"
