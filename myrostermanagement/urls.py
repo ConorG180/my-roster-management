0df6e7d5c3730c2561ce19ac8a40c5a00477f017
Conor <conor.ganly@yahoo.com>
2022-10-10 17:18:07 +0000

    Reformat paths in urls.py and add path to delete role

--- myrostermanagement/urls.py
+++ myrostermanagement/urls.py
@@ -1,32 +1,19 @@
-"""myrostermanagement URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from roles.views import role, add_role, edit_role
+from roles import views as role_views
 from home.views import home
-from employees.views import employee
-from workshifts.views import workshift
+from employees import views as employee_views
+from workshifts import views as workshift_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
-    path('role/', role, name="role_table"),
-    path('add-role/', add_role, name="add_role"),
-    path('add-role/add', add_role, name="add_role"),
-    path('edit-role/<role_id>', edit_role, name="edit"),
-    path('employee/', employee, name="employee"),
-    path('workshift/', workshift, name="workshift"),
+    path('role/', role_views.role, name="role_table"),
+    path('add-role/', role_views.add_role, name="add_role"),
+    path('add-role/add', role_views.add_role, name="add_role"),
+    path('edit-role/<role_id>', role_views.edit_role, name="edit_role"),
+    path('delete-role/<role_id>', role_views.delete_role, name="delete_role"),
+    path('employee/', employee_views.employee, name="employee"),
+    path('workshift/', workshift_views.workshift, name="workshift"),
+    # url - function - namespace: used in hrefs
 ]
