efc88aa498c7f590f85c79f19f187210491d53e7
Conor <conor.ganly@yahoo.com>
2022-10-11 17:53:29 +0000

    Create view function to render employee table

--- employees/views.py
+++ employees/views.py
@@ -1,9 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Employee
 
 def employee(request):
-    employees = Employee.objects.all()
+
+    employees = Employee.objects.all().order_by("employee_id")
+    fields = [field for field in Employee._meta.fields]
+    employee_list = employees.values_list()
+    employee_value_indexes = range(len(fields))
+
+    def employee_value_finder():
+        all_employee_values = []
+        for employee in employee_list:
+            for index in employee_value_indexes:
+                all_employee_values.append(employee[index])
+        return all_employee_values
+
     context = {
-        "employees": employees
+        "fields": fields,
+        "all_employee_values": employee_value_finder(),
+        "num_of_employees": list(range(employees.count())),
+        "col_num": len(fields),
+        "table_name": "Employees",
+        "table_item_name": "Employee"
     }
     return render(request, "employee.html", context)
+
+
+def add_employee(request):
+    return request(request, "workshifts.html")
+
+
+def edit_employee(request, employee_id):
+    return redirect("index")
+
+
+def delete_employee(request, employee_id):
+    return redirect("index")
