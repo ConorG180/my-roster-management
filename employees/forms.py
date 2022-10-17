48213acf2fe8f74efa9a1de79a0969e724a1d91a
Conor <conor.ganly@yahoo.com>
2022-10-17 01:55:58 +0000

    Change inputs and add placeholders to employee form

--- employees/forms.py
+++ employees/forms.py
@@ -1,8 +1,15 @@
 from django import forms
+from phonenumber_field.formfields import PhoneNumberField
 from .models import Employee
 
+class DateInput(forms.DateInput):
+    input_type = "date"
 
 class Employee_form(forms.ModelForm):
+    date_of_birth = forms.DateField(widget=DateInput)
+    phone_number = PhoneNumberField(widget=forms.TextInput(attrs={"placeholder": "e.g. +353872703824"}))
+    start_date = forms.DateField(widget=DateInput)
+    email = forms.EmailField()
     class Meta:
         model = Employee
         fields = ["employee_id", "first_name", "last_name", "date_of_birth", "gender", "role", "pps_number", "phone_number", "email", "start_date"]
