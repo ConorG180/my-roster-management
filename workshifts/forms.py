0d0e4b0a46a2f51f8651eef2791a45879e8d08d8
Conor <conor.ganly@yahoo.com>
2022-10-17 01:59:47 +0000

    Change inputs on workshifts form

--- workshifts/forms.py
+++ workshifts/forms.py
@@ -2,7 +2,20 @@ from django import forms
 from .models import Workshift
 
 
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+
+class TimeInput(forms.TimeInput):
+    input_type = "time"
+
+
 class Workshift_form(forms.ModelForm):
+    start_date = forms.DateField(widget=DateInput)
+    start_time = forms.TimeField(widget=TimeInput)
+    end_date = forms.DateField(widget=DateInput)
+    end_time = forms.TimeField(widget=TimeInput)
+
     class Meta:
         model = Workshift
         fields = ["workshift_id", "employee_id", "start_time", "start_date", "end_time", "end_date", "role_id"]
