2c95d147816add622dba14939f0c9e0587217da6
Conor <conor.ganly@yahoo.com>
2022-10-17 01:59:11 +0000

    Change inputs on role form

--- roles/forms.py
+++ roles/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Role
 
+
+class DecimalInput(forms.NumberInput):
+    input_type = "number"
+
+
 class Role_form(forms.ModelForm):
+    hourly_wage = forms.DecimalField(widget=DecimalInput)
+
     class Meta:
         model = Role
         fields = ["role_id", "title", "hourly_wage"]
