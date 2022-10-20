5ccfdafe140a6defc217423c78f6a6766ac2c438
Conor <conor.ganly@yahoo.com>
2022-10-20 19:51:40 +0000

    Create testing file for employee models

---
+++ employees/tests-employee-models.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from .models import Employee
+
+class TestEmployeeView(TestCase):
+
+    def test_pps_number_is_unique_in_employee_model(self):
+        unique_constraint = Employee._meta.get_field("pps_number").unique
+        self.assertTrue(unique_constraint)
+    
+    def test_email_is_unique_in_employee_model(self):
+        unique_constraint = Employee._meta.get_field("email").unique
+        self.assertTrue(unique_constraint)
