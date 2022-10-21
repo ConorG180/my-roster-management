198ee3a2be910cf40234121b394bd19ff792c7c0
Conor <conor.ganly@yahoo.com>
2022-10-21 14:48:24 +0000

    Format code in employees/models.py

--- employees/models.py
+++ employees/models.py
@@ -1,10 +1,12 @@
+"""Employee model"""
 from django.db import models
-from roles.models import Role
 from phonenumber_field.modelfields import PhoneNumberField
-# Create your models here.
+from roles.models import Role
 
 
 class Employee(models.Model):
+    """Class for employee model"""
+
     GENDER_CHOICES = [
         ('M', 'Male'),
         ('F', 'Female'),
@@ -15,13 +17,16 @@ class Employee(models.Model):
     last_name = models.CharField(max_length=50)
     date_of_birth = models.DateField()
     gender = models.CharField(max_length=11, choices=GENDER_CHOICES)
-    role = models.ForeignKey(Role, on_delete=models.CASCADE, related_name="role")
+    role = models.ForeignKey(
+        Role, on_delete=models.CASCADE, related_name="role"
+        )
     pps_number = models.CharField(max_length=9, unique=True)
     phone_number = PhoneNumberField()
     email = models.CharField(max_length=70, unique=True)
     start_date = models.DateField()
 
     class Meta:
+        """Control ordering for employee model"""
         ordering = ["start_date"]
 
     def __str__(self):
