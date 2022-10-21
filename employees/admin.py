85ce1668c553980fd601ea3f920b29e9a5477af2
Conor <conor.ganly@yahoo.com>
2022-10-21 14:49:56 +0000

    Format code in apps.py and admin.py in employees app

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,4 @@
+"""Admin panel for employees app"""
 from django.contrib import admin
 from .models import Employee
 
@@ -5,8 +6,11 @@ admin.site.site_header = "MyRosterManagement Dashboard"
 
 
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ("employee_id", "first_name", "last_name", "role", "phone_number")
+    """Class for customising employees admin view."""
+    list_display = ("employee_id", "first_name",
+                    "last_name", "role", "phone_number")
     list_filter = ("first_name", "last_name", "role",)
     search_fields = ("first_name", "last_name", "role",)
+
 
 admin.site.register(Employee, EmployeeAdmin)
--- employees/apps.py
+++ employees/apps.py
@@ -1,6 +1,8 @@
+"""app module for employees app"""
 from django.apps import AppConfig
 
 
 class EmployeesConfig(AppConfig):
+    """EmployeesConfig class"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'employees'
