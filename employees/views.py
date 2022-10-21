d81798f97b67b0cf28c6eb74593f389b2a690cf6
Conor <conor.ganly@yahoo.com>
2022-10-21 17:20:10 +0000

    Format code in employees/views.py

--- employees/views.py
+++ employees/views.py
@@ -1,30 +1,53 @@
-from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
+"""Employee view"""
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Employee
 from .forms import Employeeform
 
 
-def employee(request):
+def employee_table(request):
+    """Function for employee table view"""
 
     employees = Employee.objects.all().order_by("employee_id")
     fields = [field for field in Employee._meta.fields]
     employee_list = employees.values_list()
     employee_value_indexes = range(len(fields))
-    hidden_fields = ["date_of_birth", "gender", "pps_number", "phone_number", "email"]
+    hidden_fields = [
+        "date_of_birth",
+        "gender",
+        "pps_number",
+        "phone_number",
+        "email"
+    ]
 
     def employee_value_finder():
+        """Loops through list of employees and then grabs each value from
+        employee by looping again (A nested loop) through the employee.
+        The nested loop will always be equal to the number of values of
+        the employee, as the nested loop is defined as the length of the
+        fields
+        """
+
         all_employee_values = []
         for employee in employee_list:
             for index in employee_value_indexes:
                 all_employee_values.append(employee[index])
         return all_employee_values
-    
+
     def get_hidden_values():
+        """Goes through column which we have defined as hidden
+        , and then takes that column and gets all values from
+        that field/column of all objects in the model.
+        Then, we loop through the hidden values,
+        and append each hidden to a list value to a list.
+        Then we return that list,
+        which is called "hidden_values_list" """
+
         hidden_columns = hidden_fields
         hidden_values_list = []
         for hidden_column in hidden_columns:
             hidden_values = Employee.objects.values(hidden_column)
             for hidden_value in hidden_values:
-                hidden_values_list.append(hidden_value[hidden_column])                
+                hidden_values_list.append(hidden_value[hidden_column])
         return hidden_values_list
 
     context = {
@@ -41,6 +64,7 @@ def employee(request):
 
 
 def add_employee(request):
+    """Add record to table """
 
     # Make sure user is authorised to add records
     if request.user.is_staff is False:
@@ -48,7 +72,7 @@ def add_employee(request):
             "action": "add new employees"
         }
         return render(request, "account/signup_closed.html", context)
-        
+
     if request.method == "POST":
         form = Employeeform(request.POST)
         if form.is_valid():
@@ -72,6 +96,7 @@ def add_employee(request):
 
 
 def edit_employee(request, employee_id):
+    """Edit record in table """
 
     # Make sure user is authorised to edit records
     if request.user.is_staff is False:
@@ -104,12 +129,13 @@ def edit_employee(request, employee_id):
 
 
 def delete_employee(request, employee_id):
+    """Delete record in table """
     if request.user.is_staff is False:
         context = {
             "action": "delete employees"
         }
         return render(request, "account/signup_closed.html", context)
-        
+
     employee = get_object_or_404(Employee, employee_id=employee_id)
     employee.delete()
     return redirect("employee")
--- myrostermanagement/urls.py
+++ myrostermanagement/urls.py
@@ -1,3 +1,5 @@
+"""Handles all urls in project"""
+
 from django.contrib import admin
 from django.urls import path, include
 from roles import views as role_views
@@ -13,7 +15,7 @@ urlpatterns = [
     path('add-role/add', role_views.add_role, name="add_role"),
     path('edit-role/<role_id>', role_views.edit_role, name="edit_role"),
     path('delete-role/<role_id>', role_views.delete_role, name="delete_role"),
-    path('employee/', employee_views.employee, name="employee"),
+    path('employee/', employee_views.employee_table, name="employee"),
     path('add-employee/add', employee_views.add_employee, name="add_employee"),
     path('edit-employee/<employee_id>', employee_views.edit_employee, name="edit_employee"),
     path('delete-employee/<employee_id>', employee_views.delete_employee, name="delete_employee"),
@@ -21,7 +23,11 @@ urlpatterns = [
     path('add-workshift/', workshift_views.add_workshift, name="add_workshift"),
     path('add-workshift/add', workshift_views.add_workshift, name="add_workshift"),
     path('edit-workshift/<workshift_id>', workshift_views.edit_workshift, name="edit_workshift"),
-    path('delete-workshift/<workshift_id>', workshift_views.delete_workshift, name="delete_workshift"),
+    path(
+        'delete-workshift/<workshift_id>',
+        workshift_views.delete_workshift,
+        name="delete_workshift"
+    ),
     path('accounts/', include("allauth.urls"))
     # url - function - namespace: used in hrefs
 ]
