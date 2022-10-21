06c3dca3ba9dcb2b69b58fab9c129bb89cc08785
Conor <conor.ganly@yahoo.com>
2022-10-21 17:34:31 +0000

    Format code in apps.py and admin.py in workshifts app

--- workshifts/admin.py
+++ workshifts/admin.py
@@ -1,11 +1,20 @@
+"""Admin panel for workshifts app"""
 from django.contrib import admin
 from .models import Workshift
 # Register your models here.
 
 
 class WorkshiftAdmin(admin.ModelAdmin):
-    list_display = ("employee_id", "role_id", "start_time", "end_time")
-    list_filter = ("employee_id", "role_id", "start_time", "created_on")
+    """Class for customising workshifts admin view."""
+    list_display = (
+        "employee_id", "role_id",
+        "start_time", "end_time"
+    )
+    list_filter = (
+        "employee_id", "role_id",
+        "start_time", "created_on"
+    )
     search_fields = ("employee_id", "role_id",)
+
 
 admin.site.register(Workshift, WorkshiftAdmin)
--- workshifts/apps.py
+++ workshifts/apps.py
@@ -1,6 +1,8 @@
+"""app module for workshifts app"""
 from django.apps import AppConfig
 
 
 class WorkshiftsConfig(AppConfig):
+    """WorkshiftsConfig class"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'workshifts'
