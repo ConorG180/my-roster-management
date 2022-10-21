58cd34b8f652a85f6bcffd30390ef07c4c41e02a
Conor <conor.ganly@yahoo.com>
2022-10-21 17:33:57 +0000

    Format code in workshifts/models.py

--- workshifts/models.py
+++ workshifts/models.py
@@ -1,3 +1,4 @@
+"""Workshift model"""
 from django.db import models
 from roles.models import Role
 from employees.models import Employee
@@ -5,16 +6,27 @@ from employees.models import Employee
 
 
 class Workshift(models.Model):
+    """Class for workshift model"""
+
     workshift_id = models.BigAutoField(primary_key=True)
-    employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="employee_id_workshift_set")
+    employee_id = models.ForeignKey(
+        Employee, on_delete=models.CASCADE,
+        related_name="employee_id_workshift_set"
+    )
     start_date = models.DateField()
     start_time = models.TimeField()
     end_date = models.DateField()
     end_time = models.TimeField()
-    role_id = models.ForeignKey(Role, on_delete=models.CASCADE, related_name="role_id_workshift_set") 
+    role_id = models.ForeignKey(
+        Role,
+        on_delete=models.CASCADE,
+        related_name="role_id_workshift_set"
+    )
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
+        """Control ordering for workshift model"""
+
         ordering = ["-created_on"]
 
     def __str__(self):
