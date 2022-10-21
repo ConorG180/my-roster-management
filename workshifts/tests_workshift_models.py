b54fa35bf4bb1f2a7498bea058b8be71c134d98f
Conor <conor.ganly@yahoo.com>
2022-10-21 17:31:54 +0000

    Format code in tests_workshift_models.py

--- workshifts/tests_workshift_models.py
+++ workshifts/tests_workshift_models.py
@@ -1,8 +1,13 @@
+"""Tests for models in workshifts app."""
 from django.test import TestCase
 from .models import Workshift
 
+
 class TestWorkshiftModel(TestCase):
+    """Class for testing workshift models."""
 
     def test_created_on_is_automatic_in_employee_model(self):
+        """Test if created_on is automatic."""
+
         auto_now_add = Workshift._meta.get_field("created_on").auto_now_add
         self.assertTrue(auto_now_add)
