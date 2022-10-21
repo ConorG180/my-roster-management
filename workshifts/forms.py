f1f403160f552e74d59c3ed621f3c670aa3d30fb
Conor <conor.ganly@yahoo.com>
2022-10-21 17:33:28 +0000

    Format code in workshifts/forms.py

--- workshifts/forms.py
+++ workshifts/forms.py
@@ -1,21 +1,36 @@
+"""Forms in workshifts app."""
 from django import forms
 from .models import Workshift
 
 
 class DateInput(forms.DateInput):
+    """Controls input style of date fields"""
     input_type = "date"
 
 
 class TimeInput(forms.TimeInput):
+    """Controls input style of time fields"""
     input_type = "time"
 
 
 class WorkshiftForm(forms.ModelForm):
+    """Customize fields for the workshift form"""
+
     start_date = forms.DateField(widget=DateInput)
     start_time = forms.TimeField(widget=TimeInput)
     end_date = forms.DateField(widget=DateInput)
     end_time = forms.TimeField(widget=TimeInput)
 
     class Meta:
+        """Set fields to appear in employee form"""
+
         model = Workshift
-        fields = ["workshift_id", "employee_id", "start_time", "start_date", "end_time", "end_date", "role_id"]
+        fields = [
+            "workshift_id",
+            "employee_id",
+            "start_time",
+            "start_date",
+            "end_time",
+            "end_date",
+            "role_id"
+        ]
