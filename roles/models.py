e3021d54fe6e68319fd1714731f527cff14477dd
Conor <conor.ganly@yahoo.com>
2022-10-21 18:31:11 +0000

    Format code in roles/models.py

--- roles/models.py
+++ roles/models.py
@@ -1,11 +1,19 @@
+"""Roles model"""
 from django.db import models
 
 
 # Create your models here.
 class Role(models.Model):
+    """Class for workshift model"""
+
     role_id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=50)
-    hourly_wage = models.DecimalField(null=True, blank=True, max_digits=5, decimal_places=2)
+    hourly_wage = models.DecimalField(
+        null=True,
+        blank=True,
+        max_digits=5,
+        decimal_places=2
+    )
 
     def __str__(self):
         return f"{self.title}"
