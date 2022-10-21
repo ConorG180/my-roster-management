0a956f3ae18f28f5d66d7e7d8c726128dd3ceebe
Conor <conor.ganly@yahoo.com>
2022-10-21 14:48:06 +0000

    Format code in employees/forms.py

--- employees/forms.py
+++ employees/forms.py
@@ -1,18 +1,29 @@
+"""Forms in employee app."""
 from django import forms
 from phonenumber_field.formfields import PhoneNumberField
 from .models import Employee
 
 
 class DateInput(forms.DateInput):
+    """Controls input style of date fields"""
     input_type = "date"
 
 
 class Employeeform(forms.ModelForm):
+    """Customize fields for the employee form"""
+
     date_of_birth = forms.DateField(widget=DateInput)
-    phone_number = PhoneNumberField(widget=forms.TextInput(attrs={"placeholder": "e.g. +353872703824"}))
+    phone_number = PhoneNumberField(widget=forms.TextInput(
+        attrs={"placeholder": "e.g. +353872703824"}))
     start_date = forms.DateField(widget=DateInput)
     email = forms.EmailField()
 
     class Meta:
+        """Set fields to appear in employee form"""
+
         model = Employee
-        fields = ["employee_id", "first_name", "last_name", "date_of_birth", "gender", "role", "pps_number", "phone_number", "email", "start_date"]
+        fields = [
+                "employee_id", "first_name", "last_name", "date_of_birth",
+                "gender", "role", "pps_number", "phone_number", "email",
+                "start_date"
+                ]
