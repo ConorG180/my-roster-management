e9f1a56993253d27c8b137b93b2f57d08d330906
Conor <conor.ganly@yahoo.com>
2022-10-21 18:30:45 +0000

    Format code in roles/forms.py

--- roles/forms.py
+++ roles/forms.py
@@ -1,14 +1,20 @@
+"""Forms in roles app."""
 from django import forms
 from .models import Role
 
 
 class DecimalInput(forms.NumberInput):
+    """Controls input style of Decimal fields"""
+
     input_type = "number"
 
 
 class RoleForm(forms.ModelForm):
+    """Customize fields for the workshift form"""
+
     hourly_wage = forms.DecimalField(widget=DecimalInput)
 
     class Meta:
+        """Set fields to appear in employee form"""
         model = Role
         fields = ["role_id", "title", "hourly_wage"]
